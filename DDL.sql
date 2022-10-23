4b4a7dbe2157c8f81cb65bc007edcc8ab35fc55c
Inbenglable <1321156731@qq.com>
2022-10-23 11:50:02 +0800

    edit readme.md
    finish DDL
    
    correct some mistakes in ER_gram ,saved as "ER graph_edit"

--- DDL.sql
+++ DDL.sql
@@ -40,12 +40,6 @@ create table item(
     price int
 );
 
-create table ship_container_connection(
-    ship_id int references ship(id),
-    container_code varchar references container(code),
-    primary key (ship_id,container_code)
-);
-
 create table route(
     id int primary key ,
     retrieval_city int references city(id),
@@ -54,38 +48,34 @@ create table route(
      delivery_city_id int references city(id)
 );
 
-create table retrieve(
+create table logs(
     id int primary key ,
+    log_time date,
+    item_id int references item(id),
+    tax_info_id int references tax_info(id),
+    route_id int references route(id)
+);
+
+create table retrieve(
+    log_id int primary key references logs(id),
     courier_id int references courier(id),
     start_date date
 );
 
 create table delivery(
-        id int primary key ,
+    log_id int primary key references logs(id),
     courier_id int references courier(id),
     finish_date date
 );
 
 create table export(
-    id int primary key ,
+     log_id int primary key references logs(id),
     ship_id int references ship(id),
     container_code varchar references container(code),
     export_date date
 );
 
 create table import(
-    id int primary key ,
+    log_id int primary key references logs(id),
      import_date date
 );
-
-create table logs(
-    id int primary key ,
-    log_time date,
-    item_id int references item(id),
-    tax_info_id int references tax_info(id),
-    retrieve_id int references retrieve(id),
-    export_id int references export(id),
-    import_id int references import(id),
-    delivery_id int references delivery(id),
-    route_id int references route(id)
-)
